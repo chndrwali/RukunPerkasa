a3617a9d76cf078574a5fe812327e910d3d86591
chndrwali <chndrwali@gmail.com>
2024-02-22 17:38:08 +0700

    add: styling product details

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,10 +1,22 @@
-interface IParams {
-  producId?: string;
+import Container from '@/app/components/Container';
+import { product } from '@/utils/product';
+import ProductDetails from './ProductDetails';
+
+interface IPrams {
+  productId?: string;
 }
 
-const Product = ({ params }: { params: IParams }) => {
+const Product = ({ params }: { params: IPrams }) => {
   console.log('params', params);
-  return <div>Product Page</div>;
+  return (
+    <div className="p-8">
+      <Container>
+        {product.map((product: any) => {
+          return <ProductDetails key={product.id} data={product} />;
+        })}
+      </Container>
+    </div>
+  );
 };
 
 export default Product;
