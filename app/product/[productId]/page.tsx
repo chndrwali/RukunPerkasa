d5806943d2b642d52e7159d9db9e064476ac3332
chndrwali <chndrwali@gmail.com>
2024-02-22 14:39:30 +0700

    add: product details

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,10 @@
+interface IParams {
+  producId?: string;
+}
+
+const Product = ({ params }: { params: IParams }) => {
+  console.log('params', params);
+  return <div>Product Page</div>;
+};
+
+export default Product;
