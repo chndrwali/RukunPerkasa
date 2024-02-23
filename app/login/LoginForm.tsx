1d2675bcfef27287990fa85092037c458e755436
chndrwali <chndrwali@gmail.com>
2024-02-23 19:53:19 +0700

    add: login page

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,7 +1,46 @@
-'use client'
+'use client';
+
+import { useState } from 'react';
+import Heading from '../components/Heading';
+import Input from '../components/inputs/Input';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Button from '../components/Button';
+import Link from 'next/link';
+import { AiOutlineGoogle } from 'react-icons/ai';
 
 const LoginForm = () => {
-    return (  );
-}
- 
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+  };
+  return (
+    <>
+      <Heading center title="Masuk" />
+      <Button outline label="Masuk dengan Google" onClick={() => {}} icon={AiOutlineGoogle} />
+      <hr className="bg-slate-300 w-full h-[px]" />
+      <Input id="email" label="Masukan Email" disabled={isLoading} register={register} errors={errors} required />
+      <Input id="password" label="Masukan Password" disabled={isLoading} register={register} errors={errors} required type="password" />
+      <Button label={isLoading ? 'Mengirim data...' : 'Masuk'} onClick={handleSubmit(onSubmit)} />
+      <p className="text-sm text-center">
+        Tidak mempunyai akun?{' '}
+        <Link href="/register" className="underline">
+          Daftar sekarang
+        </Link>
+      </p>
+    </>
+  );
+};
+
 export default LoginForm;
