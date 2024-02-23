d20bb74138d60d5c8444069c4b23e5b3ad18acb4
chndrwali <chndrwali@gmail.com>
2024-02-23 17:57:20 +0700

    add: display total amount in UI

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -4,6 +4,7 @@ import { toast } from 'react-hot-toast';
 
 type CartContextType = {
   cartTotalQty: number;
+  cartTotalAmount: number;
   cartProducts: CartProductType[] | null;
   handleAddProductToCart: (product: CartProductType) => void;
   handleRemoveProductFromCart: (product: CartProductType) => void;
@@ -148,6 +149,7 @@ export const CartContextProvider = (props: Props) => {
 
   const value = {
     cartTotalQty,
+    cartTotalAmount,
     cartProducts,
     handleAddProductToCart,
     handleRemoveProductFromCart,
