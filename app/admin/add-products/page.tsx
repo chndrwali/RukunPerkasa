c91714e2b1843bb71fe3dfcce8f7cc3186221125
chndrwali <chndrwali@gmail.com>
2024-02-26 05:40:56 +0700

    add: check admin

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,8 +1,15 @@
 import Container from '@/app/components/Container';
 import FormWrap from '@/app/components/FormWrap';
 import AddProductForm from './AddProductForm';
+import getCurrentUser from '@/actions/getCurrentUser';
+import NullData from '@/app/components/NullData';
 
-const AddProducts = () => {
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title="Ups! akses di tolak" />;
+  }
   return (
     <div className="p-8 ">
       <Container>
