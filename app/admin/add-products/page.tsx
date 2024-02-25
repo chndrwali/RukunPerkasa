7a53fb0a7ba561d35042d68112fcfac7bbb2f42f
chndrwali <chndrwali@gmail.com>
2024-02-26 04:41:35 +0700

    add: add product form

--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,5 +1,17 @@
+import Container from '@/app/components/Container';
+import FormWrap from '@/app/components/FormWrap';
+import AddProductForm from './AddProductForm';
+
 const AddProducts = () => {
-  return <div>Add Products</div>;
+  return (
+    <div className="p-8 ">
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
+    </div>
+  );
 };
 
 export default AddProducts;
