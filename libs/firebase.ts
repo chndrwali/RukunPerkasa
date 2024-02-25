127ff0e1c55534712e71f449c5ed2fc1c73c5dbd
chndrwali <chndrwali@gmail.com>
2024-02-26 01:31:43 +0700

    add: firebase configuration

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-CPEdXA1y5hf5xe78JSgkviznneL3twM',
+  authDomain: 'rukun-perkasa-vid.firebaseapp.com',
+  projectId: 'rukun-perkasa-vid',
+  storageBucket: 'rukun-perkasa-vid.appspot.com',
+  messagingSenderId: '184351888538',
+  appId: '1:184351888538:web:9268a57e6db87351197b23',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
