c2602425537d3f76302e1d34622089afa5953eb8
chndrwali <chndrwali@gmail.com>
2024-02-26 13:03:20 +0700

    add: text area

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,7 +1,40 @@
 'use client';
 
+import Heading from '@/app/components/Heading';
+import Input from '@/app/components/inputs/Input';
+import TextArea from '@/app/components/inputs/TextArea';
+import { useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
+
 const AddProductForm = () => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      description: '',
+      brand: '',
+      category: '',
+      inStock: false,
+      images: [],
+      price: '',
+    },
+  });
+  return (
+    <>
+      <Heading title="Tambahkan Produk" center />
+      <Input id="name" label="Nama Produk" disabled={isLoading} register={register} errors={errors} required />
+      <Input id="price" label="Harga Produk" disabled={isLoading} register={register} errors={errors} type="number" required />
+      <Input id="brand" label="Brand" disabled={isLoading} register={register} errors={errors} required />
+      <TextArea id="description" label="Deskripsi Produk" disabled={isLoading} register={register} errors={errors} required />
+    </>
+  );
 };
 
 export default AddProductForm;
