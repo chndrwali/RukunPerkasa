84b02e5868b1711a61335c91ccc40a74f88e3616
chndrwali <chndrwali@gmail.com>
2024-02-28 10:31:47 +0700

    feat: footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,59 +1,99 @@
 import Link from 'next/link';
 import Container from '../Container';
-import FooterList from './FooterList';
-import { MdFacebook } from 'react-icons/md';
-import { AiFillTwitterCircle, AiFillInstagram, AiFillGithub } from 'react-icons/ai';
+import Image from 'next/image';
+import { Redressed } from 'next/font/google';
+import { footer } from '@/utils/footer';
+import moment from 'moment';
+import { FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 
+const redressed = Redressed({ subsets: ['latin'], weight: ['400'] });
 const Footer = () => {
   return (
-    <footer
-      className="
-  bg-brown-500
-  text-slate-200
-  text-sm 
-  mt-16
-  "
-    >
+    <footer className="bg-slate-800">
       <Container>
-        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
-          <FooterList>
-            <h3 className="text-base font-bold mb-2"> Kategori Toko</h3>
-            <Link href="#">Kusen</Link>
-            <Link href="#">Pintu</Link>
-            <Link href="#">Jendela</Link>
-            <Link href="#">Furniture</Link>
-            <Link href="#">Meja</Link>
-          </FooterList>
-          <FooterList>
-            <h3 className="text-base font-bold mb-2">Customer Service</h3>
-            <Link href="#">Contact Us</Link>
-            <Link href="#">Shoping Policy</Link>
-            <Link href="#">Return & Exchanges</Link>
-            <Link href="#">Watches</Link>
-            <Link href="#">FAQs</Link>
-          </FooterList>
-          <div className="w-full md:w-1/3 mb-6 md:mb-0">
-            <h3 className="text-base font-bold mb-2">Tentang Kami</h3>
-            <p className="mb-2"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo aperiam veritatis eum, doloribus vel placeat quia ad aspernatur impedit excepturi.</p>
-            <p>&copy; {new Date().getFullYear()} Rukun Perkasa | All Right Reserved</p>
-          </div>
-          <FooterList>
-            <h3 className="text-base font-bold mb-2">Follow Us</h3>
-            <div className="flex gap-2">
-              <Link href="#">
-                <MdFacebook size={24} />
-              </Link>
-              <Link href="#">
-                <AiFillTwitterCircle size={24} />
-              </Link>
-              <Link href="#">
-                <AiFillInstagram size={24} />
-              </Link>
-              <Link href="#">
-                <AiFillGithub size={24} />
-              </Link>
+        <div className="container p-6 mx-auto">
+          <div className="lg:flex">
+            <div className="w-full -mx-6 lg:w-2/5">
+              <div className="px-6">
+                <Link href="/" className={`${redressed.className} flex items-center space-x-3 font-bold text-2xl`}>
+                  <Image src="/logo/rukunperkasa.png" width="32" height={32} className="h-8" alt="Rukun Perkasa Logo" />
+                  <span className="self-center whitespace-nowrap text-brown-500">{footer.brand}</span>
+                </Link>
+
+                <p className="max-w-sm mt-2 text-gray-50">{footer.SEO}</p>
+
+                <div className="flex mt-6 -mx-2">
+                  <a href={footer.viewOnWhatsapp.url} target="_blank" rel="noreferrer" className="mx-2 text-gray-50 hover:text-blue-600 hover:underline" title={footer.viewOnWhatsapp.title} aria-label={footer.viewOnWhatsapp.title}>
+                    <FaWhatsapp />
+                  </a>
+
+                  <a href={footer.facebook.url} target="_blank" rel="noreferrer" className="mx-2 text-gray-50 hover:text-blue-600 hover:underline" title={footer.facebook.title} aria-label={footer.facebook.title}>
+                    <FaFacebook />
+                  </a>
+                  <a href={footer.linkedin.url} target="_blank" rel="noreferrer" className="mx-2 text-gray-50 hover:text-blue-600 hover:underline" title={footer.linkedin.title} aria-label={footer.linkedin.title}>
+                    <FaLinkedin />
+                  </a>
+                  <a href={footer.instagram.url} target="_blank" rel="noreferrer" className="mx-2 text-gray-50 hover:text-blue-600 hover:underline" title={footer.instagram.title} aria-label={footer.instagram.title}>
+                    <FaInstagram />
+                  </a>
+                </div>
+              </div>
             </div>
-          </FooterList>
+
+            <div className="mt-6 lg:mt-0 lg:flex-1">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div>
+                  <h3 className="text-gray-50 uppercase ">{footer.about.title}</h3>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.about.company}
+                  </a>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400 ">
+                    {footer.about.career}
+                  </a>
+                </div>
+
+                <div>
+                  <h3 className="text-gray-50 uppercase ">{footer.woodtype.title}</h3>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.woodtype.wood}
+                  </a>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.woodtype.wood1}
+                  </a>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.woodtype.wood2}
+                  </a>
+                </div>
+
+                <div>
+                  <h3 className="text-gray-50 uppercase ">{footer.product.title}</h3>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.product.kat}
+                  </a>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.product.kat1}
+                  </a>
+                  <a href="#" className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">
+                    {footer.product.kat2}
+                  </a>
+                </div>
+
+                <div>
+                  <h3 className="text-gray-50 uppercase ">{footer.contact.title}</h3>
+                  <span className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">{footer.contact.no}</span>
+                  <span className="block mt-2 text-sm text-gray-50 hover:underline hover:text-brown-400">{footer.contact.email}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr className="h-px my-6 bg-gray-200 border-none dark:bg-gray-700" />
+
+          <div>
+            <p className="text-center text-gray-50">
+              &copy; {moment().format('YYYY')} {footer.copy}
+            </p>
+          </div>
         </div>
       </Container>
     </footer>
---
+++ utils/footer.ts
@@ -0,0 +1,43 @@
+export const footer = {
+  brand: 'Rukun Perkasa',
+  copy: 'Rukun Perkasa | All rights reserved.',
+  SEO: 'Bergabunglah dengan lebih dari 5.000 orang lainnya dan jangan pernah lewatkan produk kusen baru, furniture, dan lainnya.',
+  about: {
+    title: 'Tentang Kami',
+    company: 'Company',
+    career: 'Career',
+  },
+  woodtype: {
+    title: 'Tipe Kayu',
+    wood: 'Jati',
+    wood1: 'Kamper',
+    wood2: 'Meranti',
+  },
+  product: {
+    title: 'Produk',
+    kat: 'Kusen Pintu',
+    kat1: 'Kusen Jendela',
+    kat2: 'Furniture',
+  },
+  contact: {
+    title: 'Kontak',
+    no: '+62 89652648201',
+    email: 'rukunperkasa@gmail.com',
+  },
+  viewOnWhatsapp: {
+    title: 'Chat on Whatsapp',
+    url: 'https://wa.me/6289652648201',
+  },
+  facebook: {
+    title: 'View On facebook',
+    url: 'https://facebook.com/rukunperkasa',
+  },
+  linkedin: {
+    title: 'View On Linkedin',
+    url: 'https://linkedin.com/in/rukunperkasa',
+  },
+  instagram: {
+    title: 'View On Instagram',
+    url: 'https://instagram.com/rukunperkasa',
+  },
+};
